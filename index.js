6a40d68eeee695a1f5542b7409ab1648a643e1e1
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-07-04 18:23:36 +0530

    Setup Express server, views, and dynamic profile routing with sample JSON data.

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const express = require("express") ; 
-const app = express() ; 
-const path =require("path") ; 
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8080;
 
-let port = 8080 ; 
+const data = require("./data/data.json");
 
-app.set("view engine" , "ejs") ; 
-app.set("views" , path.join(__dirname, "views")) ; 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.listen(port , ()=> {
-    console.log(`app listning on port ${port}`);
-    
-}) ; 
+app.get("/ig/:username", (req, res) => {
+  const { username } = req.params;
+  const userData = data[username];
+  if (userData) {
+    res.render("instagram", { username, userData });
+  } else {
+    res.send("User not found");
+  }
+});
 
-app.get("/" , (req, res)=>{
-    res.render("home.ejs")
-})
-
-app.get("/ig/:username" , (req ,res)=> {
-    let {username} = req.params ; 
-    res.render("instagram.ejs" , {username})
-
-
-})
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
